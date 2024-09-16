f3baff94b6ed6d5dcb1e3ab085872d619b5fbe9c
GeorgeBastosFerreiraFH <114554305+GeorgeBastosFerreiraFH@users.noreply.github.com>
2024-09-16 17:34:26 -0300

    Add files via upload

---
+++ atv01.py
@@ -0,0 +1,36 @@
+# saudacao  = 'Hello world'
+# print(saudacao)
+
+# Atividade Prática
+
+# nome = input('Digite seu nome: ')
+# idade = input('Digite sua idade: ')
+# altura = input('Digite sua altura: ')
+
+# print(f'Seu nome é {nome} você tem {idade} anos e {altura} metros!')
+
+
+idade1 = input('Digite a primeira idade: ')
+idade2 = input('Digite a segunda idade: ')
+
+if idade1 > idade2:
+    print(f'A idade {idade1} é maior que a {idade2}')
+
+elif idade1 < idade2:
+    print(f'A idade {idade1} é menor que a {idade2}')
+
+else:
+    print(f'A idade {idade1} é igual a {idade2}')
+
+
+
+palavra1 = str(input('Digita aqui a primeira palavra: '))
+palavra2 = str(input('Digite aqui a segunda palavra: '))
+
+resultado = (palavra1 == palavra2)
+
+print(resultado)
+
+
+# Verificação de Maioridade e Habilitação:
+
